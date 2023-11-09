acf3eb1e782385d8508ee32e00684e86c210cd3d
Dawood <Dawoodriaz1999@gmail.com>
2023-11-09 12:52:45 +0300

    All User SignUp is working

---
+++ Bidayat/main_app/forms.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+from .models import Profile 
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+        
+        
+class ProfileSignUp(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image','type','service']
+        
+        
--- Bidayat/main_app/views.py
+++ Bidayat/main_app/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from django.views.generic.edit import CreateView , UpdateView, DeleteView
+from django.views.generic import ListView,DetailView
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Profile
+from .forms import CreateUserForm , ProfileSignUp
 
 # Create your views here.
 
@@ -8,3 +20,31 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+
+
+
+# views.py
+# views.py
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        profileForm = ProfileSignUp(request.POST, request.FILES)
+        if form.is_valid() and profileForm.is_valid():
+            user = form.save()
+            profile = profileForm.save(commit=False)
+            profile.user = user
+            profile.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid Signup'
+            print(form.errors)  # Print form errors to console
+            print(profileForm.errors)  # Print profileForm errors to console
+
+    form = CreateUserForm()
+    profileForm = ProfileSignUp()
+    context = {'form': form, 'profileForm': profileForm, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
