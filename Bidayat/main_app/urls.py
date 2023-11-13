062046f18dfa20bd9b4d665017ff69ce97c959ea
Amira Zahid <[amira.web.media@gmail.com]>
2023-11-13 15:25:55 +0300

    add work page style done

--- Bidayat/main_app/urls.py
+++ Bidayat/main_app/urls.py
@@ -7,7 +7,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns=[
   path('',views.home, name='home'),
-  path('about/',views.about,name='about'),
   path('categories/', views.categories_index, name='index'),
   path('categories/<int:category_id>', views.categories_detail, name='categorydetail'),
 
